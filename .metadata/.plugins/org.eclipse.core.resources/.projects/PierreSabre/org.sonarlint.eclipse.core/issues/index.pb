bf4611d8849ee97e6530c4d05c0dc3a012845f8d
Mathis Ravera <rvm3963a@ensfsi.onmicrosoft.com>
2023-10-23 18:56:04 +0200

    creer humain

---
+++ .metadata/.plugins/org.eclipse.core.resources/.projects/PierreSabre/org.sonarlint.eclipse.core/issues/index.pb
@@ -0,0 +1,7 @@
+
+D
+src/module-info.java,c\d\cdc434daeee688944ced38ff1c9875422db88382
+K
+src/personnages/Humain.java,0\2\0201d85d44dd6801ca5c00314354b592073de1f3
+M
+src/histoire/HistoireTP4.java,e\e\ee374c8067107bf554b32b9187357772d02d4b4e
---
+++ PierreSabre/src/histoire/HistoireTP4.java
@@ -0,0 +1,17 @@
+package histoire;
+import personnages.Humain;
+public class HistoireTP4 {
+	
+	
+	
+	
+	public static void main(String[] argv) {
+		Humain marco = new Humain("Prof", "kombucha", 54);
+		marco.direBonjour();
+		marco.acheter("une boisson", 12);
+		marco.boire();
+		marco.acheter("jeu", 2);
+		marco.acheter("kimono", 40);
+	}
+
+}
