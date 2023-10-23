bf5e22fc511dd5f0027136e96490f10a6cb3eca6
Mathis Ravera <rvm3963a@ensfsi.onmicrosoft.com>
2023-10-23 19:50:43 +0200

    ronin 2.3

---
+++ PierreSabre/src/personnages/principaux/Ronin.java
@@ -0,0 +1,13 @@
+package personnages.principaux;
+import personnages.Humain;
+
+public class Ronin extends Humain {
+	
+	private int honneur = 1;
+	
+	public Ronin(String nom, String boisson, int argent) {
+		super(nom, boisson, argent);
+	}
+	
+
+}
